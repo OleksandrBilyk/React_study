60b2511d767a223edbebb168fbe65b2de4ad9bfb
Oleksandr <biluksanya@ukr.net>
2023-11-02 14:10:18 +0200

    Lesson 4 task1 todo

--- src/components/TodosContainer/Todo.js
+++ src/components/TodosContainer/Todo.js
@@ -4,7 +4,7 @@ import style from "./Todo.module.css"
 const Todo = ({todo}) => {
     const {id, title, completed} = todo;
     return (
-        <div className={style.task}>
+        <div className={completed.toString() === 'true' ? style.true : style.false}>
             <div>id: {id}</div>
             <div className={style.title}>title: {title}</div>
             <div>completed: {`${completed}`}</div>
